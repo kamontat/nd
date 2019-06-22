d38a9d415ee648d80b36edb330184607e562b3ca
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-06-22 20:02:34 +0700

    fix(logger): remove invalid empty array

--- modules/nd-logger/index.ts
+++ modules/nd-logger/index.ts
@@ -4,15 +4,16 @@ import Package from "./package.json";
 export default class LoggerService {
   public static log(log: Logger, message: any, ...args: any[]) {
     log.debug.log = console.log.bind(console);
-    log.debug(message, args);
+
+    log.debug(message, ...args);
   }
 
   public static warn(log: Logger, message: any, ...args: any[]) {
-    log.extend("warn").debug(message, args);
+    log.extend("warn").debug(message, ...args);
   }
 
   public static error(log: Logger, message: any, ...args: any[]) {
-    log.extend("error").debug(message, args);
+    log.extend("error").debug(message, ...args);
   }
 }
 
