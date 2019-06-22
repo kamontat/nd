9234fb5ec8f1b9d8cba9e6384a02278860ad3105
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-06-22 23:01:54 +0700

    impr(logger): add enable and disable log dynamically

--- modules/nd-logger/index.ts
+++ modules/nd-logger/index.ts
@@ -1,7 +1,17 @@
+import debug from "debug";
+import { config } from "nd-config";
+
 import Logger from "./models/Logger";
 import Package from "./package.json";
 
 export default class LoggerService {
+  public static console = {
+    log(message?: any, ...other: any[]) {
+      const level = config.get("output.level");
+      if (level !== "0") console.log(message, ...other);
+    },
+  };
+
   public static log(log: Logger, message: any, ...args: any[]) {
     log.debug.log = console.log.bind(console);
 
@@ -14,6 +24,14 @@ export default class LoggerService {
 
   public static error(log: Logger, message: any, ...args: any[]) {
     log.extend("error").debug(message, ...args);
+  }
+
+  public static enable(name: string) {
+    debug.enable(name);
+  }
+
+  public static disable() {
+    debug.disable();
   }
 }
 
