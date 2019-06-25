18ab31607cecfe0ceabca8855f665c0b70de72aa
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-06-25 13:58:40 +0700

    impr(error): update constants name
    [BREAKCHANGE]

--- modules/nd-error/constants.ts
+++ modules/nd-error/constants.ts
@@ -4,43 +4,49 @@ export interface ExceptionState {
   exit: boolean;
 }
 
-export const CLI_Exception: ExceptionState = {
+export const ERR_CLI: ExceptionState = {
   code: "CLI",
   name: "commandline exception",
   exit: true,
 };
 
-export const CFG_Exception: ExceptionState = {
+export const ERR_CFG: ExceptionState = {
   code: "CFG",
   name: "configuration error",
   exit: true,
 };
 
-export const HLP_Exception: ExceptionState = {
+export const ERR_HLP: ExceptionState = {
   code: "HLP",
   name: "helper and apis error",
   exit: true,
 };
 
-export const NLV_Exception: ExceptionState = {
+export const ERR_NLV: ExceptionState = {
   code: "NLV",
   name: "Novel error",
   exit: true,
 };
 
-export const LOG_Exception: ExceptionState = {
+export const WRN_NLV: ExceptionState = {
+  code: "NLV",
+  name: "Novel warning",
+  exit: false,
+};
+
+export const ERR_LOG: ExceptionState = {
   code: "LOG",
   name: "Logging error",
   exit: true,
 };
 
-export const SCT_Exception: ExceptionState = {
+export const ERR_SCT: ExceptionState = {
   code: "SCT",
   name: "security and authentication error",
   exit: true,
 };
 
-export const GNL_Exception: ExceptionState = {
+export const ERR_GNL: ExceptionState = {
   code: "GNL",
   name: "general exception",
   exit: true,
