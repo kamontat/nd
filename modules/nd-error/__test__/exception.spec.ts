98ce988369bfc00eb66cf18d8ae472ad6708929d
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-06-25 13:59:06 +0700

    chore(test): fix test error duet obreak change

--- modules/nd-error/__test__/exception.spec.ts
+++ modules/nd-error/__test__/exception.spec.ts
@@ -2,7 +2,7 @@ import * as chai from "chai";
 import "mocha";
 import LoggerMock from "nd-logger/models/__test__/LoggerMock";
 
-import { GNL_Exception } from "../constants";
+import { ERR_GNL } from "../constants";
 import Exception from "../model";
 chai.should();
 
@@ -13,31 +13,31 @@ const rootName = "Exception";
 describe(rootName, function() {
   describe("Models", function() {
     it("should able to create new exception", function() {
-      const exp = new Exception(GNL_Exception);
+      const exp = new Exception(ERR_GNL);
       exp.should.not.be.undefined;
     });
 
     it("should able to receive exception code", function() {
-      const exp = new Exception(GNL_Exception);
+      const exp = new Exception(ERR_GNL);
       exp.name.should.be.equal("GNL");
     });
 
     it("should custom message via .description()", function() {
       const message = "hello world";
-      const exp = new Exception(GNL_Exception);
+      const exp = new Exception(ERR_GNL);
       exp.description(message).message.should.be.equal(message);
     });
 
     it("should custom message via .description()", function() {
       const message = "hello world";
-      const exp = new Exception(GNL_Exception);
+      const exp = new Exception(ERR_GNL);
 
       exp.description(message).message.should.be.equal(message);
     });
 
     it("should print to console", function() {
       const message = "hello world";
-      const exp = new Exception(GNL_Exception);
+      const exp = new Exception(ERR_GNL);
 
       exp.description(message).print(
         new LoggerMock((event, args: any[]) => {
