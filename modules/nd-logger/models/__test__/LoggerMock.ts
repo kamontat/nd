bcc2a148f19a202e8cee1460d579f4700fa29a38
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-06-25 13:27:36 +0700

    chore(test): final mocking log model

--- modules/nd-logger/models/__test__/LoggerMock.ts
+++ modules/nd-logger/models/__test__/LoggerMock.ts
@@ -1,13 +1,20 @@
 import Logger from "../Logger";
 
-type Callback = (format: any, ...args: any[]) => void;
+type Callback = (event: LoggerEvent, format: any, ...args: any[]) => void;
+
+export type LoggerEvent = "extend" | "debug";
 
 export default class LoggerMock extends Logger {
   constructor(private callback: Callback) {
     super("nd:test");
   }
 
+  public extend(namespace: string) {
+    this.callback("extend", namespace);
+    return new LoggerMock(this.callback); // create new logger instance
+  }
+
   public debug(format: any, ...args: any[]) {
-    return this.callback(format, ...args);
+    return this.callback("debug", format, ...args);
   }
 }
