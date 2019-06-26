2a935e4fd5c9d02ed95bac105045b4d14b15162b
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-06-26 09:27:11 +0700

    chore(exception): add more test

--- modules/nd-error/__test__/exception.spec.ts
+++ modules/nd-error/__test__/exception.spec.ts
@@ -3,7 +3,7 @@ import * as chai from "chai";
 import "mocha";
 import LoggerMock from "nd-logger/models/__test__/LoggerMock";
 
-import { ERR_GNL } from "../constants";
+import { ERR_CLI, ERR_GNL, ERR_LOG, WRN_NLV } from "../constants";
 import Exception from "../model";
 chai.should();
 
@@ -45,6 +45,54 @@ describe(rootName, function() {
           args.should.not.be.empty;
         }),
       );
+    });
+
+    it("should print to consol in warning", function() {
+      const message = "hello world";
+      const exp = new Exception(WRN_NLV);
+
+      exp.description(message).print(
+        new LoggerMock((event, args: any[]) => {
+          console.log(event);
+          console.log(args);
+
+          event.should.not.be.undefined;
+          args.should.not.be.empty;
+        }),
+      );
+    });
+  });
+
+  describe("Exception builder", function() {
+    it("should create build new exception by static build", function() {
+      const exp = Exception.build(ERR_GNL);
+
+      exp.name.should.not.be.undefined;
+      exp.message.should.be.equal(ERR_GNL.name);
+    });
+
+    it("should able to cast Error to new exception", function() {
+      const errMessage = "custom error message";
+      const err = new Error(errMessage);
+      const exp = Exception.cast(err);
+
+      exp.message.should.be.equal(errMessage);
+      exp.name.should.be.equal(ERR_GNL.code); // default base exception
+    });
+
+    it("should able to cast own exception to exception", function() {
+      const exp1 = Exception.build(ERR_LOG);
+      const exp2 = Exception.cast(exp1);
+
+      exp1.should.be.equal(exp2);
+    });
+
+    it("should able to cast Error to new exception (with custom code)", function() {
+      const errMessage = "custom error message";
+      const err = new Error(errMessage);
+      const exp = Exception.cast(err, { base: ERR_CLI });
+
+      exp.name.should.be.equal(ERR_CLI.code); // default base exception
     });
   });
 });
