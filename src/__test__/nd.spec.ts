7ecc6d75d916662ba929a5e099e2f4ad7e80544f
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-06-27 16:54:24 +0700

    chore(core): add more test

---
+++ src/__test__/nd.spec.ts
@@ -0,0 +1,49 @@
+import * as chai from "chai";
+import "mocha";
+import { Commandline, Option } from "nd-commandline-interpreter";
+import { CommandlineEvent } from "nd-commandline-interpreter";
+import { MockArguments } from "nd-commandline-interpreter/__test__/high-level.spec";
+import MockConfiguration from "nd-config/models/__test__/MockConfiguration";
+
+import { BuildCommandline } from "../nd";
+chai.should();
+
+// const addContext = require("mochawesome/addContext");
+
+const rootName = "ND root command";
+
+describe(rootName, function() {
+  const CLI_ERR = new Error("commandline didn't initial on time");
+  let cli: Commandline | undefined;
+  this.beforeAll(async () => {
+    cli = await BuildCommandline(new Commandline("nd", "for test purpose only"), new MockConfiguration());
+  });
+
+  describe("demo nd command in programming", function() {
+    it("should get version option", function(done) {
+      if (!cli) throw CLI_ERR;
+
+      const customEvent = new CommandlineEvent();
+      customEvent.on("globalOption", function(opt: Option) {
+        opt.name.should.be.equal("version");
+        done();
+      });
+
+      cli.event = customEvent;
+      cli.run(MockArguments("--version"));
+    });
+
+    it("should get version option", function(done) {
+      if (!cli) throw CLI_ERR;
+
+      const customEvent = new CommandlineEvent();
+      customEvent.on("globalOption", function(opt: Option) {
+        opt.name.should.be.equal("help");
+        done();
+      });
+
+      cli.event = customEvent;
+      cli.run(MockArguments("--help"));
+    });
+  });
+});
