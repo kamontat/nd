8b01046028afe62a99d482944cfd9eff80c7b043
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-06-28 23:38:32 +0700

    impr(logger): add new namespace to support local caches

--- modules/nd-logger/constants.ts
+++ modules/nd-logger/constants.ts
@@ -4,6 +4,7 @@ export const LOGGER_ROOT = new Logger("nd");
 
 export const LOGGER_CLI = LOGGER_ROOT.extend("cli");
 export const LOGGER_CLI_BUILDER = LOGGER_CLI.extend("builder");
+export const LOGGER_CLI_CONFIG = LOGGER_CLI.extend("config");
 
 export const LOGGER_SECURITY = LOGGER_ROOT.extend("security");
 
