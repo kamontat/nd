8b43caf7974a14aadad434e2c649270a8e6c5414
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-09 19:51:07 +0700

    impr(error): now error log format is depend on node env

--- modules/nd-error/model.ts
+++ modules/nd-error/model.ts
@@ -3,12 +3,23 @@ import Logger from "nd-logger/models/Logger";
 
 import { ERR_GNL, ExceptionState } from "./constants";
 
+declare var __NODE_ENV__: string;
+
 export default class Exception extends Error {
+  private env: string;
+
+  private printProduction<T extends Logger>(log: T) {
+    if (this._code.exit) LoggerService.error(log, this.message);
+    else LoggerService.warn(log, this.message);
+    return this;
+  }
+
   constructor(private _code: ExceptionState) {
     super(``);
 
     this.name = _code.code;
     this.message = Exception.buildMessage(_code);
+    this.env = __NODE_ENV__;
   }
 
   public description(d: string) {
@@ -17,6 +28,7 @@ export default class Exception extends Error {
   }
 
   public print<T extends Logger>(log: T) {
+    if (this.env === "production") return this.printProduction(log);
     if (this._code.exit) LoggerService.error(log, "%O", this);
     else LoggerService.warn(log, "%O", this);
     return this;
