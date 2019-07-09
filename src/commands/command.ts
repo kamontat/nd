88ad442ca140df09c6e5ee49c4eb4bbe50a30c68
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-09 19:47:14 +0700

    refac(file): rename

---
+++ src/commands/command.ts
@@ -0,0 +1,25 @@
+import { Command, Commandline, Option, SubCommand } from "nd-commandline-interpreter";
+import { IConfiguration } from "nd-config";
+import LoggerService, { LOGGER_CLI } from "nd-logger";
+
+import { COMMAND_INFORMATION, VERSION, VERSION_FULL } from "../constants/content";
+
+export default (cli: Commandline, _: IConfiguration) => {
+  cli.command(
+    Command.build("command", false, ({ self }) => {
+      LoggerService.console.log(COMMAND_INFORMATION(self.name));
+    }).sub(
+      SubCommand.build("version", false, ({ self, apis }) => {
+        LoggerService.log(LOGGER_CLI, `${self.name} start initial setting command`);
+
+        if (apis.config.get("version.detail")) LoggerService.console.log(VERSION_FULL());
+        else LoggerService.console.log(VERSION());
+      }).option(
+        Option.build("detail", false, ({ apis }) => {
+          LoggerService.log(LOGGER_CLI, `Start version as a fully detail`);
+          apis.config.set("version.detail", true);
+        }),
+      ),
+    ),
+  );
+};
