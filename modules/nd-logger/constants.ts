cc92f93b47d117e41536b6fff3008111e907aaeb
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-11 18:22:50 +0700

    impr(logger): add more log namespace for download and update

--- modules/nd-logger/constants.ts
+++ modules/nd-logger/constants.ts
@@ -9,3 +9,9 @@ export const LOGGER_CLI_CONFIG = LOGGER_CLI.extend("config");
 export const LOGGER_SECURITY = LOGGER_ROOT.extend("security");
 
 export const LOGGER_CONFIG = LOGGER_ROOT.extend("config");
+
+export const LOGGER_DOWNLOADER = LOGGER_ROOT.extend("download");
+
+export const LOGGER_NOVEL = LOGGER_ROOT.extend("novel");
+export const LOGGER_NOVEL_DOWNLOADER = LOGGER_NOVEL.extend("download");
+export const LOGGER_NOVEL_UPDATER = LOGGER_NOVEL.extend("update");
