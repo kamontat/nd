4ad7319cb6d42a514a11a6f3c2328a644db1a354
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-15 16:44:24 +0700

    impr(decoder): update decoder to pass encode directly

--- modules/nd-decoder/index.ts
+++ modules/nd-decoder/index.ts
@@ -6,12 +6,11 @@ import Package from "./package.json";
 
 export type DecodeSupport = "TIS-620" | "UTF-8";
 
-export default (str: string, encode: DecodeSupport) => {
-  const buf = Buffer.from(str);
+export default (buf: Buffer, encode: DecodeSupport) => {
   LoggerService.log(LOGGER_DOWNLOADER_DECODER, `string encode format is ${encode}`);
 
   try {
-    return decode(buf, encode === "UTF-8" ? "UTF-8" : "ISO-8859-11");
+    return decode(buf, encode);
   } catch (e) {
     throw ExceptionService.cast(e, { base: ERR_DCR });
   }
