e45491e5c55394086a98b0c40ce88220d41d21e8
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-16 11:38:37 +0700

    chore(helper): add more log and log

--- modules/nd-helper/models/Optional.ts
+++ modules/nd-helper/models/Optional.ts
@@ -1,3 +1,5 @@
+import LoggerService, { LOGGER_MODEL } from "nd-logger";
+
 export class Optional<T, R = T> {
   private _isTransform: boolean;
   private result: R | undefined;
@@ -6,18 +8,22 @@ export class Optional<T, R = T> {
   }
 
   public static of<T, R = T>(t: T | undefined | null) {
+    LoggerService.log(LOGGER_MODEL, `insert object %O`, t);
     return new Optional<T, R>(t);
   }
 
   public or(r: R): R {
+    LoggerService.log(LOGGER_MODEL, `called or method: %O`, this);
     if (this._isTransform) return this.result || r;
     else return ((this._optional as unknown) as R) || r;
   }
 
   public transform(fn: (t: T) => R): this {
+    LoggerService.log(LOGGER_MODEL, `called transform method: %O`, this);
     if (this._optional) {
+      this._isTransform = true;
       this.result = fn(this._optional);
-    } else this._isTransform = true;
+    }
     return this;
   }
 }
