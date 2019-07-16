f75e3626465f61960c89dd6f8ddb3c3d93c57766
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-16 17:49:46 +0700

    impr(helper): remove logger in optional model

--- modules/nd-helper/models/Optional.ts
+++ modules/nd-helper/models/Optional.ts
@@ -1,5 +1,3 @@
-import LoggerService, { LOGGER_MODEL } from "nd-logger";
-
 export class Optional<T, R = T> {
   private _isTransform: boolean;
   private result: R | undefined;
@@ -8,18 +6,18 @@ export class Optional<T, R = T> {
   }
 
   public static of<T, R = T>(t: T | undefined | null) {
-    LoggerService.log(LOGGER_MODEL, `insert object %O`, t);
+    // LoggerService.log(LOGGER_MODEL, `insert object %O`, t);
     return new Optional<T, R>(t);
   }
 
   public or(r: R): R {
-    LoggerService.log(LOGGER_MODEL, `called or method: %O`, this);
+    // LoggerService.log(LOGGER_MODEL, `called or method: %O`, this);
     if (this._isTransform) return this.result || r;
     else return ((this._optional as unknown) as R) || r;
   }
 
   public transform(fn: (t: T) => R): this {
-    LoggerService.log(LOGGER_MODEL, `called transform method: %O`, this);
+    // LoggerService.log(LOGGER_MODEL, `called transform method: %O`, this);
     if (this._optional) {
       this._isTransform = true;
       this.result = fn(this._optional);
