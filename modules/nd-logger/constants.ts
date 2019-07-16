d73d329df30fd8a082a95bb69c6a896348182f07
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-16 11:39:06 +0700

    impr(logger): add debug log, this should remove before deploy

--- modules/nd-logger/constants.ts
+++ modules/nd-logger/constants.ts
@@ -2,6 +2,9 @@ import Logger from "./models/Logger";
 
 export const LOGGER_ROOT = new Logger("nd");
 
+export const LOGGER_DEBUG = LOGGER_ROOT.extend("debug");
+export const LOGGER_MODEL = LOGGER_DEBUG.extend("model");
+
 export const LOGGER_CLI = LOGGER_ROOT.extend("cli");
 export const LOGGER_CLI_BUILDER = LOGGER_CLI.extend("builder");
 export const LOGGER_CLI_CONFIG = LOGGER_CLI.extend("config");
@@ -15,5 +18,6 @@ export const LOGGER_DOWNLOADER_MANAGER = LOGGER_DOWNLOADER.extend("manager");
 export const LOGGER_DOWNLOADER_DECODER = LOGGER_DOWNLOADER.extend("decoder");
 
 export const LOGGER_NOVEL = LOGGER_ROOT.extend("novel");
+export const LOGGER_NOVEL_BUILDER = LOGGER_NOVEL.extend("builder");
 export const LOGGER_NOVEL_DOWNLOADER = LOGGER_NOVEL.extend("download");
 export const LOGGER_NOVEL_UPDATER = LOGGER_NOVEL.extend("update");
