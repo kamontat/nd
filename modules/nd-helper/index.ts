514e649691634193d82403143db083ad0722aeb0
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-22 10:37:43 +0700

    chore(helper): add array utils as a export method

--- modules/nd-helper/index.ts
+++ modules/nd-helper/index.ts
@@ -1,7 +1,8 @@
+import ArrayUtils from "./apis/array";
 import PathUtils from "./apis/path";
 import TimeUtils from "./apis/time";
 import Package from "./package.json";
 
-export { Package, TimeUtils, PathUtils };
+export { Package, TimeUtils, PathUtils, ArrayUtils };
 
 export { Optional } from "./models/Optional";
