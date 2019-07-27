499cbcc3bdd458af5ccb56f21f71571997a49fcd
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-27 15:47:42 +0700

    feat(file): custom file exception callback

--- modules/nd-file/index.ts
+++ modules/nd-file/index.ts
@@ -4,8 +4,21 @@ import path from "path";
 
 import Package from "./package.json";
 
+type ErrorType = "file-exist" | "folder-not-found";
+type ErrorCallback = (path: string) => void;
+
+const EmptyCallback = () => {};
+
 export default class FileManager {
+  private _errors: Map<ErrorType, ErrorCallback>;
+
+  public get path() {
+    return this.directory;
+  }
+
   constructor(private directory: string, name?: string) {
+    this._errors = new Map();
+
     if (name) this.directory = this.buildPath(name); // append name to directory
 
     if (!fs.existsSync(this.directory)) {
@@ -25,22 +38,29 @@ export default class FileManager {
     }
   }
 
+  public onError(type: ErrorType, callback: ErrorCallback) {
+    this._errors.set(type, callback);
+  }
+
   public save(content: string, fileName: string, opts?: { force?: boolean }) {
     const defaultOptions = {
       force: false,
       ...opts,
     };
+
     LoggerService.log(LOGGER_FILE, `save to file with option=%O`, defaultOptions);
 
     const p = this.buildPath(fileName);
     if (!fs.existsSync(p) || defaultOptions.force) {
       LoggerService.log(LOGGER_FILE, `force save file even it already exist`);
       fs.writeFileSync(p, content, { encoding: "utf8", mode: 0o666, flag: "w" });
+    } else {
+      LoggerService.log(LOGGER_FILE, `file already exist`);
+      if (this._errors.has("file-exist")) {
+        const callback = this._errors.get("file-exist") || EmptyCallback;
+        callback(p);
+      }
     }
-  }
-
-  public get path() {
-    return this.directory;
   }
 
   private buildPath(name: string) {
