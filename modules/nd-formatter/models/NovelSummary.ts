929fa67e99279b02b1f6dbbaf82d00ff6e9c964e
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-27 17:42:23 +0700

    feat(formatter): add support novel printing with changelog

--- modules/nd-formatter/models/NovelSummary.ts
+++ modules/nd-formatter/models/NovelSummary.ts
@@ -1,19 +1,19 @@
 import { ArrayUtils, TimeUtils } from "nd-helper";
 import { Colorize } from "nd-logger";
-import { ChapterStatus, Novel } from "nd-novel";
+import { ChapterStatus, History, Novel } from "nd-novel";
 
 import { IDefaultConfigFormat, IFormatter } from "./IFormatter";
 
-interface NovelConfigFormat extends IDefaultConfigFormat {
+interface INovelConfigFormat extends IDefaultConfigFormat {
   chapter: boolean;
   path: string;
   short: boolean;
 }
 
-export class NovelSummary implements IFormatter<NovelConfigFormat, Novel> {
+export class NovelSummary implements IFormatter<INovelConfigFormat, Novel> {
   private __summary: string;
 
-  private _config?: NovelConfigFormat;
+  private _config?: INovelConfigFormat;
   private _obj?: Novel;
 
   constructor() {
@@ -36,7 +36,7 @@ export class NovelSummary implements IFormatter<NovelConfigFormat, Novel> {
     return summary;
   }
 
-  public config(conf: NovelConfigFormat) {
+  public config(conf: INovelConfigFormat) {
     this._config = conf;
     return this;
   }
@@ -62,13 +62,41 @@ chapters    =  ${ArrayUtils.ReadableArray(
       )}
 update at   =  ${Colorize.date(TimeUtils.FormatDate(TimeUtils.GetDate(this._obj.updateAt)))}
 download at =  ${Colorize.date(TimeUtils.FormatDate(TimeUtils.GetDate(this._obj.downloadAt)))}
-${this._config && `path        =  ${Colorize.path(this._config.path)}`}
-`);
+${this._config && `path        =  ${Colorize.path(this._config.path)}`}`);
+
+      if (this._config && this._config.chapter === true) {
+        this._appendSummary(this._section("Chapters"));
+        Array.from(this._obj.chapters).forEach(c => this._appendSummary(c.toString({ color: true, long: true })));
+
+        this._appendSummary(this._section("Histories"));
+        const events = History.Get().events;
+        events.forEach(n => this._appendSummary(n.toString({ color: true })));
+      }
     }
   }
 
-  private _appendSummary(a: any) {
+  private _appendSummary(a: any, newline: boolean = true) {
     this.__summary += a;
+    if (newline) this.__summary += "\n";
+  }
+
+  private _section(title: string) {
+    const lineSize = 35;
+
+    const left = lineSize - title.length;
+
+    let line = "";
+    for (let i = 0; i < lineSize; i++) line += "-";
+    let padding = "";
+
+    if (left > 0) {
+      for (let i = 0; i < left; i++) padding += " ";
+    }
+    return `
+# ${Colorize.dim(line)} #
+# ${Colorize.key(title)}${padding} #
+# ${Colorize.dim(line)} #
+`;
   }
 
   private getSummary() {
