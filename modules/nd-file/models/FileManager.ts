b822daba6b56d5fcb5b5c78ebcc5d2846696e98a
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-28 20:20:06 +0700

    impr(file): add support multiple thread write to file

--- modules/nd-file/models/FileManager.ts
+++ modules/nd-file/models/FileManager.ts
@@ -1,17 +1,31 @@
 import fs from "fs";
 import LoggerService, { LOGGER_FILE } from "nd-logger";
+import { ThreadManager } from "nd-thread";
 import path from "path";
+import util from "util";
 
 import { ErrorCallback, ErrorManager, ErrorType } from "./ErrorManager";
 
-export class FileManager {
-  private _error: ErrorManager;
+interface IFileOption {
+  force?: boolean;
+}
 
+interface IMultithreadValue {
+  content: string;
+  filename: string;
+  opts: IFileOption;
+}
+
+export class FileManager extends ThreadManager<undefined, IMultithreadValue, undefined> {
   public get path() {
     return this.directory;
   }
 
-  constructor(private directory: string, name?: string) {
+  private _error: ErrorManager;
+
+  constructor(private directory: string, name?: string, thread?: number) {
+    super(thread);
+
     this._error = new ErrorManager();
     if (name) this.directory = this.buildPath(name); // append name to directory
   }
@@ -44,22 +58,60 @@ export class FileManager {
     this._error.set(type, callback);
   }
 
-  public save(content: string, fileName: string, opts?: { force?: boolean }) {
-    const defaultOptions = {
-      force: false,
-      ...opts,
-    };
+  /**
+   * this method for single thread loading
+   *
+   * @param content file content
+   * @param fileName filename (without path)
+   * @param _opts saving option
+   */
+  public save(content: string, fileName: string, _opts?: IFileOption) {
+    const opts = Object.assign({ force: false }, _opts);
+    LoggerService.log(LOGGER_FILE, `save async file with option=%O`, opts);
 
-    LoggerService.log(LOGGER_FILE, `save to file with option=%O`, defaultOptions);
+    const isExist = util.promisify(fs.exists);
+    const writeFile = util.promisify(fs.writeFile);
 
     const p = this.buildPath(fileName);
-    if (!fs.existsSync(p) || defaultOptions.force) {
-      LoggerService.log(LOGGER_FILE, `force save file even it already exist`);
+
+    return isExist(p).then(exist => {
+      if (exist && opts.force) {
+        LoggerService.log(LOGGER_FILE, `force save file even it already exist`);
+        return writeFile(p, content, { encoding: "utf8", mode: 0o666, flag: "w" });
+      } else if (!exist) {
+        LoggerService.log(LOGGER_FILE, `save file to path=${p}`);
+        return writeFile(p, content, { encoding: "utf8", mode: 0o666, flag: "w" });
+      } else {
+        LoggerService.log(LOGGER_FILE, `file already exist`);
+        this._error.execute("file-exist", p);
+        return new Promise((_, rej) => rej(new Error("cannot save file because it already exist")));
+      }
+    }) as Promise<undefined>;
+  }
+
+  /**
+   * same as save but with sync function
+   *
+   * @param content
+   * @param fileName
+   * @param _opts
+   * @see FileManager.save method
+   */
+  public saveSync(content: string, fileName: string, _opts?: IFileOption) {
+    const opts = Object.assign({ force: false }, _opts);
+    LoggerService.log(LOGGER_FILE, `save sync file with option=%O`, opts);
+
+    const p = this.buildPath(fileName);
+    if (!fs.existsSync(p) || opts.force) {
       fs.writeFileSync(p, content, { encoding: "utf8", mode: 0o666, flag: "w" });
     } else {
       LoggerService.log(LOGGER_FILE, `file already exist`);
       this._error.execute("file-exist", p);
     }
+  }
+
+  protected transform(t: IMultithreadValue) {
+    return this.save(t.content, t.filename, t.opts);
   }
 
   private buildPath(name: string) {
