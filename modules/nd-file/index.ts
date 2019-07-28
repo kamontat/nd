76088df649e0527a01599933dc6f2a6a6dfe32dc
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-28 20:20:19 +0700

    chore(docs): add usage example in file

--- modules/nd-file/index.ts
+++ modules/nd-file/index.ts
@@ -1,6 +1,37 @@
 import { FileManager } from "./models/FileManager";
 import Package from "./package.json";
 
+// ---------------------------- //
+// Usage A: single thread       //
+// ---------------------------- //
+
+// const manager = new FileManager("/tmp/hello-world", "custom-path");
+// manager.load(); // you must either call load() or call name()
+
+// manager.save("hello world", "example-A.txt", { force: true }); // for promise return
+// manager.saveSync("hello world", "example-A.txt", { force: true }); // for sync method
+
+// ---------------------------- //
+// Usage B: multiple thread     //
+// ---------------------------- //
+
+// const manager = new FileManager("/tmp/multiple-thread", undefined, 12); // create 12 thread worker
+// manager.load(); // you must either call load() or call name()
+
+// manager.add({ content: "content-A", filename: "example.A.txt", opts: { force: false } });
+// manager.add({ content: "content-B", filename: "example.B.txt", opts: { force: false } });
+// manager.add({ content: "content-C", filename: "example.C.txt", opts: { force: false } });
+// manager.add({ content: "content-D", filename: "example.D.txt", opts: { force: false } });
+
+// manager
+//   .run()
+//   .then(() => {
+//     console.log("done");
+//   })
+//   .catch(e => {
+//     console.log(`Error: ${e}`);
+//   });
+
 export default FileManager;
 
 export { Package };
