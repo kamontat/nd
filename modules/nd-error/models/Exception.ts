f78099ed38d3935ef04543df4abd4ab00d98f16d
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-07-28 17:22:07 +0700

    impr(error): support stack error when load Error object

--- modules/nd-error/models/Exception.ts
+++ modules/nd-error/models/Exception.ts
@@ -14,25 +14,38 @@ export default class Exception extends Error {
     return this;
   }
 
+  private _description: string;
   private env: string;
 
-  private printProduction<T extends Logger>(log: T) {
-    if (this._exit) LoggerService.error(log, this.message);
-    else LoggerService.warn(log, this.message);
-    return this;
-  }
-
-  constructor(private _code: IExceptionState, private _description?: string, private _exit: boolean = true) {
+  constructor(private _code: IExceptionState, description?: string | Error, private _exit: boolean = true) {
     super(``);
 
-    this.name = _code.code;
-    this.message = _code.buildMessage(_exit ? MessageType.ERROR : MessageType.WARNING, _description);
     this.env = __NODE_ENV__;
+    if (typeof description === "string") this._description = description || "";
+    else {
+      this._description =
+        this.env === "production"
+          ? (description as Error).message
+          : `${(description as Error).message}\n${(description as Error).stack}`;
+    }
+
+    this.name = _code.code;
+    this.message = _code.buildMessage(_exit ? MessageType.ERROR : MessageType.WARNING, this._description);
+  }
+
+  public static cast<T extends Error>(e: T, opts?: { base?: IExceptionState }): Exception {
+    if (e instanceof Exception) return e;
+
+    return new Exception(!opts || !opts.base ? ERR_GNL : opts.base, e);
   }
 
   public description(d: string) {
     this.message = d; // override message with description
     return this;
+  }
+
+  public exit(code: number = 1) {
+    if (this._exit) process.exit(code);
   }
 
   public print<T extends Logger>(log: T) {
@@ -42,12 +55,9 @@ export default class Exception extends Error {
     return this;
   }
 
-  public exit(code: number = 1) {
-    if (this._exit) process.exit(code);
-  }
-
-  public static cast<T extends Error>(e: T, opts?: { base?: IExceptionState }): Exception {
-    if (e instanceof Exception) return e;
-    return new Exception(!opts || !opts.base ? ERR_GNL : opts.base, e.message);
+  private printProduction<T extends Logger>(log: T) {
+    if (this._exit) LoggerService.error(log, this.message);
+    else LoggerService.warn(log, this.message);
+    return this;
   }
 }
