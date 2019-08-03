86ee8fe4769ef4314d7d76de2addc7557883d0eb
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-03 16:56:40 +0700

    chore(helper): public random utils

--- modules/nd-helper/index.ts
+++ modules/nd-helper/index.ts
@@ -1,9 +1,10 @@
 import ArrayUtils from "./apis/array";
 import CheckerUtils from "./apis/checker";
 import PathUtils from "./apis/path";
+import RandomUtils from "./apis/random";
 import TimeUtils from "./apis/time";
 import Package from "./package.json";
 
-export { Package, TimeUtils, PathUtils, ArrayUtils, CheckerUtils };
+export { Package, RandomUtils, TimeUtils, PathUtils, ArrayUtils, CheckerUtils };
 
 export { Optional } from "./models/Optional";
