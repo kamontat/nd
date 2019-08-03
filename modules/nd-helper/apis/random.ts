826a3820c9af1d8a6c926120b6b960a213ca1846
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-03 16:54:42 +0700

    impr(helper): add random number and string

---
+++ modules/nd-helper/apis/random.ts
@@ -0,0 +1,21 @@
+import { pseudoRandomBytes } from "crypto";
+
+export default {
+  RandomNumber(_opts: { end?: number; start?: number }) {
+    const opt = Object.assign({ end: 10, start: 0 }, _opts);
+    return opt.start + Math.floor(Math.random() * opt.end);
+  },
+  RandomString(size = 10) {
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const number = "0123456789";
+    const __array = [lower, upper, number];
+
+    let str = "";
+    for (let i = 0; i < size; i++) {
+      const type = __array[this.RandomNumber({ start: 0, end: __array.length })];
+      str += type.charAt(this.RandomNumber({ start: 0, end: type.length }));
+    }
+    return str;
+  },
+};
