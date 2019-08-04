b31705d932669b6e7299a95fef1c8e0feb5c5899
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-04 13:36:56 +0700

    impr(logger): add history namespace

--- modules/nd-logger/constants.ts
+++ modules/nd-logger/constants.ts
@@ -18,6 +18,8 @@ export const LOGGER_DOWNLOADER = LOGGER_ROOT.extend("download");
 export const LOGGER_DOWNLOADER_MANAGER = LOGGER_DOWNLOADER.extend("manager");
 export const LOGGER_DOWNLOADER_DECODER = LOGGER_DOWNLOADER.extend("decoder");
 
+export const LOGGER_HISTORY = LOGGER_ROOT.extend("history");
+
 export const LOGGER_NOVEL = LOGGER_ROOT.extend("novel");
 export const LOGGER_NOVEL_BUILDER = LOGGER_NOVEL.extend("builder");
 export const LOGGER_NOVEL_DOWNLOADER = LOGGER_NOVEL.extend("download");
