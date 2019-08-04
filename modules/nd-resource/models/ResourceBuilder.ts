1803cc5210321e276c491ff23b691a15fd189287
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-04 12:30:51 +0700

    impr(resource): support multiple use in resource builder

--- modules/nd-resource/models/ResourceBuilder.ts
+++ modules/nd-resource/models/ResourceBuilder.ts
@@ -1,11 +1,18 @@
 import { Novel } from "nd-novel";
 
+import { RESOURCE_FILENAME } from "../constants";
+
 import { Resource } from "./Resource";
 
-export class ResourceBuilder {
-  constructor(private novel: Novel) {}
+export abstract class ResourceBuilder {
+  public static Novel = class extends ResourceBuilder {
+    constructor(private novel: Novel) {
+      super();
+    }
+    public build() {
+      return new Resource(JSON.stringify(this.novel.toJSON({ content: false })), RESOURCE_FILENAME);
+    }
+  };
 
-  public build() {
-    return new Resource(JSON.stringify(this.novel.toJSON()));
-  }
+  public abstract build(): Resource;
 }
