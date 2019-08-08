48d10683c32be7c26131085b5fc8d3cc7d72f4f3
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-08 18:18:47 +0700

    impr(path): add new generate name APIs

--- modules/nd-helper/apis/path.ts
+++ modules/nd-helper/apis/path.ts
@@ -6,24 +6,32 @@ export default {
   GetCurrentPath() {
     return process.cwd();
   },
+  Tmpname(suffix?: string) {
+    const _suffix = suffix || "tmp";
+    return `${+new Date()}-${RandomUtils.RandomString(6)}-${_suffix}`;
+  },
   Tmpdir(dirPath: string) {
     const root = path.dirname(dirPath);
-    const name = `${+new Date()}-${RandomUtils.RandomString(6)}-d`;
+    const name = this.Tmpname("d");
     return path.resolve(root, name);
   },
   Tmpfile(filePath: string, ext: string = ".tmp") {
     const root = path.dirname(filePath);
-    const name = `${+new Date()}-${RandomUtils.RandomString(6)}-d`;
+    const name = this.Tmpname("f");
     return path.resolve(root, `${name}${ext}`);
+  },
+  Cachename(name: string, suffix?: string) {
+    const _suffix = suffix || "tmp";
+    return `${name}-${RandomUtils.RandomString(3)}-${_suffix}`;
   },
   Cachedir(dirPath: string) {
     const root = path.dirname(dirPath);
-    const name = `${path.basename(dirPath)}-${RandomUtils.RandomString(3)}-d`;
+    const name = this.Cachename(path.basename(dirPath), "d");
     return path.resolve(root, name);
   },
   Cachefile(filePath: string, ext: string = ".txt") {
     const root = path.dirname(filePath);
-    const name = `${path.basename(filePath)}-${RandomUtils.RandomString(3)}-d`;
+    const name = this.Cachename(path.basename(filePath), "d");
     return path.resolve(root, `${name}${ext}`);
   },
 };
