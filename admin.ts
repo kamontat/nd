ef5e0d117ff86a32e9370f7666046b0489857bc1
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-11 18:56:07 +0700

    impr(admin): use anonymous method instead

--- admin.ts
+++ admin.ts
@@ -1,11 +1,9 @@
-import { BuildAdminCommmandline, Package } from "nd-admin";
+import { BuildAdminCommandline, Package } from "nd-admin";
 import { Commandline } from "nd-commandline-interpreter";
-import LoggerService from "nd-logger";
 
 const cli = new Commandline(Package.name, Package.description);
 
-BuildAdminCommmandline(cli, { stdin: process.stdin, stdout: process.stdout }).then(str => {
-  LoggerService.console.log(str);
-});
-
-cli.run(process.argv);
+(async () => {
+  const commandline = await BuildAdminCommandline(cli, { stdin: process.stdin, stdout: process.stdout });
+  await commandline.run(process.argv);
+})();
