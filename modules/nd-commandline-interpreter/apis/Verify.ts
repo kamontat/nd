34af90c2b409ecb698bfb990d5838e9a745de211
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-11 19:00:42 +0700

    impr(cli-builder): add support auth via server

--- modules/nd-commandline-interpreter/apis/Verify.ts
+++ modules/nd-commandline-interpreter/apis/Verify.ts
@@ -1,4 +1,5 @@
 import { IConfiguration } from "nd-config";
+import { Database, DatabaseService } from "nd-database";
 import ExceptionService, { ERR_SCT } from "nd-error";
 import { Security } from "nd-security";
 
@@ -40,9 +41,14 @@ export default {
       return false;
     }
   },
-  CheckAuthenication(config: IConfiguration): { err?: Error; secure: Security } {
+  async CheckAuthenication(config: IConfiguration): Promise<{ err?: Error; secure: Security }> {
     const secure = new Security("v1", config.get("auth.name") as string);
     if (secure.isVerified(config.get("auth.token") as string, config.get("auth.salt") as string)) {
+      if (secure.response) {
+        const err = await secure.server.isActivated();
+        if (err) return { err, secure };
+      }
+
       return { err: undefined, secure };
     } else {
       return {
