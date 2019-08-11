42a217a8feb0e6e30f9f71b820b7bf3d88dd2445
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-11 12:29:16 +0700

    impr(database): implement firebase with read/write access

--- modules/nd-database/models/Database.ts
+++ modules/nd-database/models/Database.ts
@@ -10,12 +10,34 @@ export default class Database implements IDatabase<database.DataSnapshot> {
 
   constructor(app: app.App) {
     this._db = firebase.database(app);
-    // LoggerService.log(LOGGER_FIREBASE, this._db);
+    LoggerService.log(LOGGER_FIREBASE, this._db);
+  }
+
+  public append(path: string, value: any) {
+    LoggerService.log(LOGGER_FIREBASE, `append ${value} to ${path}`);
+    const newRef = this._db.ref(path).push();
+    return newRef.set(value).catch(err => {
+      new Promise((_, rej) => rej(ExceptionService.cast(err, { base: ERR_DBO })));
+    }) as Promise<database.DataSnapshot>;
   }
 
   public read(path: string) {
     LoggerService.log(LOGGER_FIREBASE, `start read value from ${path}`);
+    return this._db
+      .ref(path)
+      .once("value")
+      .catch(err => {
+        new Promise((_, rej) => rej(ExceptionService.cast(err, { base: ERR_DBO })));
+      }) as Promise<database.DataSnapshot>;
+  }
 
-    return this._db.ref(path).once("value");
+  public write(path: string, value: any) {
+    LoggerService.log(LOGGER_FIREBASE, `start write ${value} to ${path}`);
+    return this._db
+      .ref(path)
+      .update(value)
+      .catch(err => {
+        new Promise((_, rej) => rej(ExceptionService.cast(err, { base: ERR_DBO })));
+      }) as Promise<database.DataSnapshot>;
   }
 }
