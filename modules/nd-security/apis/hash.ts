6d761183c6114fad13864141c06d7213f74bac3e
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-11 18:58:48 +0700

    impr(security): response of hash function will be uppercase

--- modules/nd-security/apis/hash.ts
+++ modules/nd-security/apis/hash.ts
@@ -1,7 +1,9 @@
 export const hash = (str: string) => {
-  return Buffer.from(str, "utf8").toString("hex");
+  return Buffer.from(str, "utf8")
+    .toString("hex")
+    .toUpperCase();
 };
 
 export const unhash = (str: string) => {
-  return Buffer.from(str, "hex").toString("utf8");
+  return Buffer.from(str.toLowerCase(), "hex").toString("utf8");
 };
