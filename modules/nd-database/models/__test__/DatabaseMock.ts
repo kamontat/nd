62614323b66e22454e4df5b06640209e4158d55e
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-11 12:30:01 +0700

    chore(database): update mock db to access memory db

--- modules/nd-database/models/__test__/DatabaseMock.ts
+++ modules/nd-database/models/__test__/DatabaseMock.ts
@@ -1,9 +1,58 @@
 import IDatabase from "../IDatabase";
 
+/**
+ * NOTES: Never tested; be aware when use this
+ */
 export default abstract class DatabaseMock implements IDatabase {
-  public abstract getValue(path: string): any;
+  private static index: number = 0;
+
+  protected __list: { [key: string]: any };
+
+  constructor() {
+    this.__list = new Map();
+  }
+
+  public append(path: string, value: any) {
+    return this.write(path.concat("/", DatabaseMock.index.toString()).replace("//", "/"), value);
+  }
 
   public read(path: string) {
-    return new Promise(res => res(this.getValue(path)));
+    return new Promise(res => {
+      let result;
+      const _arr = path.split("/");
+      _arr.forEach(a => {
+        const v = this.__list[a];
+        if (v) result = v;
+        else result = undefined;
+      });
+
+      res(result);
+    });
+  }
+
+  public write(path: string, value: any) {
+    return new Promise(res => {
+      const _arr = path.split("/");
+      if (_arr.pop() === "" && _arr.length < 0) this.__list = value;
+      this.__list = this.__setValue(this.__list, _arr, value);
+      res();
+    });
+  }
+
+  protected getValue(path: string): any {
+    return this.__list.get(path);
+  }
+
+  private __setValue(list: { [key: string]: any }, paths: string[], value: string): any {
+    const first = paths.pop();
+
+    // terminal condition
+    if (first === "" || first === undefined || first === null) {
+      return value;
+    }
+
+    const next = paths;
+
+    return this.__setValue(list[first], next, value);
   }
 }
