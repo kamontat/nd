682283315fb42893768e290d70e62c2bd11d91e3
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-12 13:38:38 +0700

    impr(novel): support preload chapter title from index page

--- modules/nd-novel/apis/dekd/getChapterList.ts
+++ modules/nd-novel/apis/dekd/getChapterList.ts
@@ -8,11 +8,26 @@ export default (parser: IParser<string, string, Cheerio>) => {
 
   parser.query("a[target=_blank]").each((_, e) => {
     const link = e.attribs.href;
+    const title = e.attribs.title;
     if (link && link.includes("viewlong")) {
       const url = new URL(`http://${link}`);
-      chapters.push(
-        new Chapter(parseInt(url.searchParams.get("id") || "", 10), parseInt(url.searchParams.get("chapter") || "", 10)),
+      const chapter = new Chapter(
+        parseInt(url.searchParams.get("id") || "", 10),
+        parseInt(url.searchParams.get("chapter") || "", 10),
       );
+
+      // preload title from title attribute
+      if (title) {
+        const title2 = title.replace("อ่าน ", "");
+
+        LoggerService.log(
+          LOGGER_NOVEL_BUILDER,
+          `preload: chapter title of ${url.searchParams.get("chapter")} is ${title2}`,
+        );
+        chapter.name = title2;
+      }
+
+      chapters.push(chapter);
     }
   });
   return chapters;
