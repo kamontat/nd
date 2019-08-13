5c8df096974b4c4a8953a942a0aa4f2f815d3513
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-13 16:37:37 +0700

    feat(cli): add fetch novel from path and show the result

--- src/commands/novel/fetch.ts
+++ src/commands/novel/fetch.ts
@@ -1,7 +1,12 @@
 import { ICommandCallback } from "nd-commandline-interpreter";
+import { config } from "nd-config";
+import FileManager from "nd-file";
 import FormatterFactory, { NovelSummary } from "nd-formatter";
+import { PathUtils } from "nd-helper";
 import LoggerService, { LOGGER_NOVEL_FETCHER } from "nd-logger";
+import { Novel } from "nd-novel";
 import { NovelBuilder } from "nd-novel";
+import { Resource } from "nd-resource";
 
 const __fetch_url = (value: number, opts: { chapter: boolean; fast: boolean; thread: number }) => {
   LoggerService.log(LOGGER_NOVEL_FETCHER, `get url/id as parameter; ${value}`);
@@ -27,8 +32,24 @@ const __fetch_url = (value: number, opts: { chapter: boolean; fast: boolean; thr
 };
 
 const __fetch_path = async (value: string, opts: { chapter: boolean; thread: number }) => {
+  config.set("novel.location", value);
+
+  const location = config.get("novel.location");
+  const fileManager = new FileManager.write(location || PathUtils.GetCurrentPath(), undefined, opts.thread);
+
   LoggerService.log(LOGGER_NOVEL_FETCHER, `get path as parameter; ${value}`);
   LoggerService.log(LOGGER_NOVEL_FETCHER, `Options: %O`, opts);
+
+  const resource = new Resource.File(fileManager.system.directory);
+  const novel = new Novel.Resource(resource);
+
+  const format = FormatterFactory.Build().get<NovelSummary>("novel");
+  const result = format
+    .save(novel)
+    .config({ chapters: true, path: fileManager.system.directory, short: true, _format: true, history: false })
+    .build();
+
+  LoggerService.console.log(result);
 };
 
 const __main: ICommandCallback = async ({ value, apis }) => {
