2c0436fb134a9af5205803df0553525aceb476d0
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-17 07:47:20 +0700

    impr(security): change the way to generate firebase name

--- modules/nd-security/apis/methods/GenerateFirebaseName.ts
+++ modules/nd-security/apis/methods/GenerateFirebaseName.ts
@@ -1,5 +1,6 @@
 import { hash } from "../hash";
 
 export default (username: string) => {
-  return hash(`${username}`);
+  const number = ("0" + Math.ceil(Math.random() * 99)).slice(-2);
+  return hash(`${username}-${number}`);
 };
