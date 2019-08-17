90c5791fe27eec6bf387c404851ef83ab88cb075
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-17 08:06:56 +0700

    feat(admin): add logger level and print when error occur

--- admin.ts
+++ admin.ts
@@ -1,9 +1,19 @@
 import { BuildAdminCommandline, Package } from "nd-admin";
 import { Commandline } from "nd-commandline-interpreter";
+import ExceptionService from "nd-error";
+import LoggerService, { LOGGER_CLI } from "nd-logger";
 
 const cli = new Commandline(Package.name, Package.description);
 
 (async () => {
-  const commandline = await BuildAdminCommandline(cli, { stdin: process.stdin, stdout: process.stdout });
-  await commandline.run(process.argv);
+  LoggerService.level(1);
+
+  try {
+    const commandline = await BuildAdminCommandline(cli, { stdin: process.stdin, stdout: process.stdout });
+    await commandline.run(process.argv);
+  } catch (e) {
+    ExceptionService.cast(e)
+      .print(LOGGER_CLI)
+      .exit(1);
+  }
 })();
