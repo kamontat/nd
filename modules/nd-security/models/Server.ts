6ca79c0880316625a7915fc6d7b81ad53965aa62
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-18 16:28:49 +0700

    chore(security): update database api changes

--- modules/nd-security/models/Server.ts
+++ modules/nd-security/models/Server.ts
@@ -1,5 +1,6 @@
 import { Database, DatabaseService } from "nd-database";
 import ExceptionService, { ERR_SCT } from "nd-error";
+import LoggerService, { LOGGER_SECURITY } from "nd-logger";
 
 import { Security } from "..";
 import { USER_PATH_ACTIVATED, USER_PATH_ROOT } from "../constants";
@@ -11,10 +12,21 @@ export default class Server {
     if (!this.security.response) return ExceptionService.build(ERR_SCT, "you didn't query authentication first.");
 
     try {
+      LoggerService.log(LOGGER_SECURITY, "connecting to database server...");
+
       const db = DatabaseService.Get<Database>();
-      const _value = await db.read(`${USER_PATH_ROOT}/${this.security.response.fbname}${USER_PATH_ACTIVATED}`);
-      const value = _value.val();
-      if (value !== true && value !== "true") {
+      const _value = await db.read(`${USER_PATH_ROOT}/${this.security.response.fbname}`);
+
+      if (!_value.exists) {
+        return ExceptionService.build(
+          ERR_SCT,
+          "your user data is not exist; contact our admin to manually add to the server",
+        );
+      }
+
+      LoggerService.log(LOGGER_SECURITY, "  - data is exist, start verifying...");
+      const value = _value.data();
+      if (!value || (value[USER_PATH_ACTIVATED] !== true && value[USER_PATH_ACTIVATED] !== "true")) {
         return ExceptionService.build(
           ERR_SCT,
           "your user is not activated; if you didn't hack our command please contact us.",
