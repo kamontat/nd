c76128b683b7514b8d04729602cecca0290f7912
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-18 16:05:02 +0700

    chore(lib): upgrade lib version and remove tabtab

--- src/commands/completion.ts
+++ src/commands/completion.ts
@@ -2,24 +2,21 @@ import { SubCommand } from "nd-commandline-interpreter";
 import { Command, Commandline } from "nd-commandline-interpreter";
 import { IConfiguration } from "nd-config";
 import LoggerService, { LOGGER_CLI } from "nd-logger";
-// TODO: make completion as a dependency instead.
-import tabtab from "tabtab";
 
 export default (cli: Commandline, _: IConfiguration) => {
   cli.command(
     Command.build("completion", false, async ({ self }) => {
       LoggerService.log(LOGGER_CLI, `${self.name} start install completion`);
-      await tabtab.install({
-        name: "nd",
-        completer: "nd",
-      });
+      // await tabtab.install({
+      //   name: "nd",
+      //   completer: "nd",
+      // });
     }).sub(
       SubCommand.build("uninstall", false, async ({ self }) => {
         LoggerService.log(LOGGER_CLI, `${self.name} start uninstall completion`);
-
-        await tabtab.uninstall({
-          name: "nd",
-        });
+        // await tabtab.uninstall({
+        //   name: "nd",
+        // });
       }),
     ),
   );
