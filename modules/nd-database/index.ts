220a29c99f5fcf7f63c008f4176f62217b5c1212
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-18 16:58:19 +0700

    feat(database): add new method initialfirebasestorage

--- modules/nd-database/index.ts
+++ modules/nd-database/index.ts
@@ -1,13 +1,17 @@
-import firebase from "firebase/app";
-
 import Database from "./models/Database";
 import IDatabase from "./models/IDatabase";
+import Storage from "./models/Storage";
 import Package from "./package.json";
 import DatabaseService from "./services/DatabaseService";
 
 export const InitialFirebaseDatabase = () => {
-  DatabaseService.Set(new Database());
-  // LoggerService.log(LOGGER_FIREBASE, "%O", DatabaseService.Get());
+  DatabaseService.Set("database", new Database());
+  // LoggerService.log(LOGGER_FIREBASE, "firebase database: %O", DatabaseService.Get());
+};
+
+export const InitialFirebaseStorage = () => {
+  DatabaseService.Set("storage", new Storage());
+  // LoggerService.log(LOGGER_FIREBASE, "firebase storage: %O", DatabaseService.Get());
 };
 
 // -------------------- //
