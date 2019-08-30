3761a7ffaf875caeb9431c8346cdf4906318b604
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-08-30 21:56:46 +0700

    impr(thread): return option is optional

--- modules/nd-thread/models/IThreadable.ts
+++ modules/nd-thread/models/IThreadable.ts
@@ -1,4 +1,7 @@
-export type EachFn<K, V, O, OO> = (element: { key: K; value: V }, opts: { option?: O; optionOnce?: OO }) => Promise<O>;
+export type EachFn<K, V, O, OO> = (
+  element: { key: K; value: V },
+  opts: { option?: O; optionOnce?: OO },
+) => Promise<O | undefined | void>;
 
 export type MapFn<K, V, R, O, OO> = (
   element: { key: K; value: V },
