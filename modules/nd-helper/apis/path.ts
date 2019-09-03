6242d08c7f24ee57ead070dee73f374fc207eac8
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-09-03 22:40:30 +0700

    impr(helper): customable on random string size in caches and tmp

--- modules/nd-helper/apis/path.ts
+++ modules/nd-helper/apis/path.ts
@@ -6,32 +6,32 @@ export default {
   GetCurrentPath() {
     return process.cwd();
   },
-  Tmpname(suffix?: string) {
+  Tmpname(suffix?: string, size: number = 6) {
     const _suffix = suffix || "tmp";
-    return `${+new Date()}-${RandomUtils.RandomString(6)}-${_suffix}`;
+    return `${+new Date()}-${RandomUtils.RandomString(size)}-${_suffix}`;
   },
-  Tmpdir(dirPath: string) {
+  Tmpdir(dirPath: string, size: number = 6) {
     const root = path.dirname(dirPath);
-    const name = this.Tmpname("d");
+    const name = this.Tmpname("d", size);
     return path.resolve(root, name);
   },
-  Tmpfile(filePath: string, ext: string = ".tmp") {
+  Tmpfile(filePath: string, ext: string = ".tmp", size: number = 6) {
     const root = path.dirname(filePath);
-    const name = this.Tmpname("f");
+    const name = this.Tmpname("f", size);
     return path.resolve(root, `${name}${ext}`);
   },
-  Cachename(name: string, suffix?: string) {
+  Cachename(name: string, suffix?: string, size: number = 3) {
     const _suffix = suffix || "tmp";
-    return `${name}-${RandomUtils.RandomString(3)}-${_suffix}`;
+    return `${name}-${RandomUtils.RandomString(size)}-${_suffix}`;
   },
-  Cachedir(dirPath: string) {
+  Cachedir(dirPath: string, size: number = 3) {
     const root = path.dirname(dirPath);
-    const name = this.Cachename(path.basename(dirPath), "d");
+    const name = this.Cachename(path.basename(dirPath), "d", size);
     return path.resolve(root, name);
   },
-  Cachefile(filePath: string, ext: string = ".txt") {
+  Cachefile(filePath: string, ext: string = ".txt", size: number = 3) {
     const root = path.dirname(filePath);
-    const name = this.Cachename(path.basename(filePath), "d");
+    const name = this.Cachename(path.basename(filePath), "f", size);
     return path.resolve(root, `${name}${ext}`);
   },
 };
