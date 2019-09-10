5cdcfe42ba67a6fd06efa00d545599207a244f7f
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-09-10 19:26:39 +0700

    impr(novel): implement auto get 'update at' when index downloaded

--- modules/nd-novel/apis/dekd/getChapterList.ts
+++ modules/nd-novel/apis/dekd/getChapterList.ts
@@ -1,7 +1,9 @@
+import { TimeUtils } from "nd-helper";
 import LoggerService, { LOGGER_NOVEL_BUILDER } from "nd-logger";
 
 import { Chapter } from "../../models/novel/Chapter";
 import { IParser } from "../../models/parser/IParser";
+import { convertChapterDate } from "../date";
 
 export default (parser: IParser<string, string, Cheerio>) => {
   const chapters: Array<Chapter> = [];
@@ -16,7 +18,7 @@ export default (parser: IParser<string, string, Cheerio>) => {
         parseInt(url.searchParams.get("chapter") || "", 10),
       );
 
-      // preload title from title attribute
+      // preload title from title attribute for v2
       if (title) {
         const title2 = title.replace("อ่าน ", "");
 
@@ -28,9 +30,26 @@ export default (parser: IParser<string, string, Cheerio>) => {
         chapter.name = title2;
       }
 
+      // preload update at
+      const span = e.prev.prev;
+      if (span && span.tagName === "span") {
+        const title = span.attribs.title;
+        if (title.includes("อัพเดทล่าสุด")) {
+          const dateString = title.replace("อัพเดทล่าสุด ", "");
+          LoggerService.log(
+            LOGGER_NOVEL_BUILDER,
+            `preload: chapter updated ${url.searchParams.get("chapter")} date is ${dateString}`,
+          );
+
+          const date = convertChapterDate(dateString);
+          chapter.updateAt = TimeUtils.GetTimestamp(date);
+        }
+      }
+
       LoggerService.log(LOGGER_NOVEL_BUILDER, `receive ${chapter.cid} from <a> tag`);
       chapters.push(chapter);
     }
   });
+
   return chapters;
 };
