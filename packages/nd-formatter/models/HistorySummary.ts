42ff57c1dcb24cef1b714352f518f7cf81402515
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-09-13 15:42:15 +0700

    feat(formatter): add history formatter

---
+++ packages/nd-formatter/models/HistorySummary.ts
@@ -0,0 +1,24 @@
+import { History } from "nd-novel";
+
+import { IDefaultConfigFormat, IFormatter } from "./IFormatter";
+
+export class HistorySummary implements IFormatter<IDefaultConfigFormat, History> {
+  private _obj?: History;
+
+  public build() {
+    let str = "";
+    if (this._obj) {
+      this._obj.events.forEach(n => (str += n.toString({ color: true })));
+    }
+    return str;
+  }
+
+  public config(_: IDefaultConfigFormat) {
+    return this;
+  }
+
+  public save(v: History) {
+    this._obj = v;
+    return this;
+  }
+}
