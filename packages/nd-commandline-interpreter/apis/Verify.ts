8ec55b16e978d03d98b49049730ea27b67ae4eb4
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-09-13 13:30:24 +0700

    refac(cli-builder): move ischecker out from verify

--- packages/nd-commandline-interpreter/apis/Verify.ts
+++ packages/nd-commandline-interpreter/apis/Verify.ts
@@ -1,64 +1,8 @@
-import { readFileSync as readFile } from "fs";
 import { IConfiguration } from "nd-config";
 import ExceptionService, { ERR_SCT } from "nd-error";
 import { Security } from "nd-security";
-import { resolve, sep } from "path";
 
 export default {
-  IsExist(n: any) {
-    if (n === undefined || n === null) return false;
-    if (typeof n === "object" && Object.keys(n).length <= 0) return false;
-
-    let str: string = n;
-    if (!this.IsString(n)) {
-      str = n.toString();
-    }
-
-    return str !== "" && str.toLowerCase() !== "null" && str.toLowerCase() !== "undefined";
-  },
-  IsString(n: any) {
-    return n instanceof String || typeof n === "string";
-  },
-  IsBoolean(n: any) {
-    if (!this.IsString(n)) return typeof n === "boolean";
-    return n.toString() === "true" || n.toString() === "false";
-  },
-  IsNumber(n?: any) {
-    if (!this.IsString(n)) return parseInt(n, 10) === n;
-    return /^\d+$/.test(n);
-  },
-  IsDecimal(n?: any) {
-    return /^\d+\.\d+$/.test(n);
-  },
-  IsId(n?: any) {
-    return this.IsNumber(n);
-  },
-  IsPath(n: any) {
-    // check is not url
-    if (this.IsUrl(n)) return false;
-
-    // check is seperate exist; assume is a file system path
-    const __arr = (n as string).match(sep);
-    return __arr && __arr.length > 0;
-  },
-  IsFileExist(n: any, filename: string) {
-    if (!this.IsPath(n)) return false;
-    try {
-      const b = readFile(resolve(n, filename));
-      return b.length > 0;
-    } catch (e) {
-      return false;
-    }
-  },
-  IsUrl(n?: any) {
-    if (!this.IsString(n)) return false;
-    try {
-      const url = new URL(n);
-      return url.protocol.includes("http") || url.protocol.includes("https");
-    } catch (e) {
-      return false;
-    }
-  },
   async CheckAuthenication(config: IConfiguration): Promise<{ err?: Error; secure: Security }> {
     const secure = new Security("v1", config.get("auth.name") as string);
     if (secure.isVerified(config.get("auth.token") as string, config.get("auth.salt") as string)) {
