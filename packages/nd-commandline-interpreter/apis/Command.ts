c23319554fb405534b7a2f7c275eb21982e431df
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-09-13 13:31:22 +0700

    chore(cli-builder): reformat code

--- packages/nd-commandline-interpreter/apis/Command.ts
+++ packages/nd-commandline-interpreter/apis/Command.ts
@@ -3,30 +3,30 @@ import CommandlineConfig from "../models/CommandlineConfig";
 import Verify from "./Verify";
 
 export default class CommandApis {
-  private static c: CommandApis;
-
-  public exit() {
-    process.exit(0);
-  }
-
-  public end() {
-    return true;
-  }
-
-  public forceExit() {
-    process.exit(5);
+  get config() {
+    return CommandlineConfig.CONST();
   }
 
   get verify() {
     return Verify;
   }
 
-  get config() {
-    return CommandlineConfig.CONST();
-  }
+  private static c: CommandApis;
 
   public static get() {
     if (!CommandApis.c) CommandApis.c = new CommandApis();
     return CommandApis.c;
+  }
+
+  public end() {
+    return true;
+  }
+
+  public exit() {
+    process.exit(0);
+  }
+
+  public forceExit() {
+    process.exit(5);
   }
 }
