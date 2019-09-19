fc6b0c1906050a882f0321af8dfd26bcf369e657
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-09-19 18:38:02 +0700

    impr(helper): log and tmp folder is dynamic name

--- packages/nd-helper/constants/files.ts
+++ packages/nd-helper/constants/files.ts
@@ -8,14 +8,14 @@ export const ND_TMP_LOCATION = resolve(`/tmp`, `nd-${__COMPILE_DATE__}`);
 
 // ------------
 
-export const TMP_DIRECTORY = resolve(ND_TMP_LOCATION, "tmp");
+export const TMP_DIRECTORY = resolve(ND_TMP_LOCATION, PathUtils.Cachename("tmp", "d", 4));
 
 export const TMPA_PATH = PathUtils.Tmpfile(TMP_DIRECTORY, ".tmpa");
 export const TMPB_PATH = PathUtils.Tmpfile(TMP_DIRECTORY, ".tmpb");
 
 // ------------
 
-export const LOG_DIRECTORY = resolve(ND_TMP_LOCATION, "logs");
+export const LOG_DIRECTORY = resolve(ND_TMP_LOCATION, PathUtils.Cachename("log", "d", 4));
 
 export const LOG_PATH = PathUtils.Tmpfile(LOG_DIRECTORY, ".log");
 export const WARN_PATH = PathUtils.Tmpfile(LOG_DIRECTORY, ".warn");
