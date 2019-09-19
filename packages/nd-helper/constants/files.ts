df4a58edd5118903776eccae36d8f02dffff0890
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-09-19 18:22:18 +0700

    impr(helper): improve tmp and log name to support apis

--- packages/nd-helper/constants/files.ts
+++ packages/nd-helper/constants/files.ts
@@ -1,21 +1,22 @@
+import { resolve } from "path";
+
+import PathUtils from "../apis/path";
+
 declare var __COMPILE_DATE__: string;
 
-export const TMP = `/tmp/nd-${__COMPILE_DATE__}`;
+export const ND_TMP_LOCATION = resolve(`/tmp`, `nd-${__COMPILE_DATE__}`);
 
-export const LOG = `${TMP}/logs`;
+// ------------
 
-export const TMP_A = `${TMP}/TA-${Math.random()
-  .toString(36)
-  .substring(7)}-${+new Date()}`;
+export const TMP_DIRECTORY = resolve(ND_TMP_LOCATION, "tmp");
 
-export const TMP_B = `${TMP}/TB-${Math.random()
-  .toString(36)
-  .substring(7)}-${+new Date()}`;
+export const TMPA_PATH = PathUtils.Tmpfile(TMP_DIRECTORY, ".tmpa");
+export const TMPB_PATH = PathUtils.Tmpfile(TMP_DIRECTORY, ".tmpb");
 
-export const LOG_A = `${LOG}/LA-${Math.random()
-  .toString(36)
-  .substring(7)}-${+new Date()}`;
+// ------------
 
-export const LOG_B = `${LOG}/LB-${Math.random()
-  .toString(36)
-  .substring(7)}-${+new Date()}`;
+export const LOG_DIRECTORY = resolve(ND_TMP_LOCATION, "logs");
+
+export const LOG_PATH = PathUtils.Tmpfile(LOG_DIRECTORY, ".log");
+export const WARN_PATH = PathUtils.Tmpfile(LOG_DIRECTORY, ".warn");
+export const ERROR_PATH = PathUtils.Tmpfile(LOG_DIRECTORY, ".error");
