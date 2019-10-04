68a03d4cf98f76dab6a762a73d770c495fb8f39a
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-10-04 22:14:51 +0700

    impr(novel): update novel apis method to return new content object

--- packages/nd-novel/apis/dekd/INovelAPIs.ts
+++ packages/nd-novel/apis/dekd/INovelAPIs.ts
@@ -1,16 +1,17 @@
 import { Chapter } from "../../models/novel/Chapter";
 import { ChapterStatus } from "../../models/novel/ChapterStatus";
 import { IParser } from "../../models/parser/IParser";
+import { HtmlEntity } from "@nd/content";
 
 export interface INovelAPIs<K = string, R = string> {
-  getChapterContent(parser: IParser<string, K, R>): string[];
+  getChapterContent(parser: IParser<string, K, R>): HtmlEntity[];
   getChapterDate(parser: IParser<string, K, R>): Date;
   getChapterList(parser: IParser<string, K, R>): Chapter[];
   getChapterName(parser: IParser<string, K, R>): string;
   getChapterStatus(parser: IParser<string, K, R>): ChapterStatus;
 
   getNovelAbstract(parser: IParser<string, K, R>): string;
-  getNovelContent(parser: IParser<string, K, R>): string[];
+  getNovelContent(parser: IParser<string, K, R>): HtmlEntity[];
   getNovelDate(parser: IParser<string, K, R>): Date;
   getNovelName(parser: IParser<string, K, R>): string;
 
--- packages/nd-novel/apis/dekd/getNovelContent.ts
+++ packages/nd-novel/apis/dekd/getNovelContent.ts
@@ -1,11 +1,12 @@
 import LoggerService, { LOGGER_NOVEL_BUILDER } from "@nd/logger";
 
 import { IParser } from "../../models/parser/IParser";
+import { HtmlEntity, HtmlEntityType } from "@nd/content";
 
 const HTML_BLACKLIST_TEXT = ["ads.dek-d.com", "min_t_comment", "Dek-D Writer APP", "BG&Picture"];
 
 export default (parser: IParser<string, string | CheerioElement, Cheerio>) => {
-  const result: string[] = [];
+  const result: HtmlEntity[] = [];
 
   parser
     .query(".prevent-user-select")
@@ -20,7 +21,7 @@ export default (parser: IParser<string, string | CheerioElement, Cheerio>) => {
         if (HTML_BLACKLIST_TEXT.filter(v => text.includes(v)).length < 1) {
           LoggerService.log(LOGGER_NOVEL_BUILDER, "novel node data: %O", text);
 
-          result.push(text);
+          result.push(new HtmlEntity({ tag: HtmlEntityType.P, content: text }));
         }
       }
     });
