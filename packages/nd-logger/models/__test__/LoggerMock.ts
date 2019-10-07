6cb4cf192135e2f4bda7eec7fe5c997195c8d02e
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-10-07 12:27:18 +0700

    chore(lint): resolve any error on some file

--- packages/nd-logger/models/__test__/LoggerMock.ts
+++ packages/nd-logger/models/__test__/LoggerMock.ts
@@ -1,5 +1,6 @@
 import Logger from "../Logger";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Callback = (event: LoggerEvent, format: any, ...args: any[]) => void;
 
 export type LoggerEvent = "extend" | "debug" | "stdlog" | "stderr";
@@ -24,6 +25,7 @@ export default class LoggerMock extends Logger {
     return new LoggerMock(this.callback); // create new logger instance
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public debug(format: any, ...args: any[]) {
     super.debug(format, ...args); // try to called super too
     return this.callback("debug", format, ...args);
