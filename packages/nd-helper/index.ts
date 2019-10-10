2c96391084296154e1716e624b33aaf3e5d99585
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-10-10 21:38:24 +0700

    chore(helper): export StringUtils from helper

--- packages/nd-helper/index.ts
+++ packages/nd-helper/index.ts
@@ -2,9 +2,11 @@ import ArrayUtils from "./apis/array";
 import PathUtils from "./apis/path";
 import RandomUtils from "./apis/random";
 import TimeUtils from "./apis/time";
+import StringUtils from "./apis/string";
+
 import Package from "./package.json";
 
-export { Package, RandomUtils, TimeUtils, PathUtils, ArrayUtils };
+export { Package, RandomUtils, TimeUtils, PathUtils, ArrayUtils, StringUtils };
 
 export { Optional } from "./models/optional";
 export { is } from "./apis/datetype";
