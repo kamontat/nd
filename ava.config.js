e9912316901469b1a6464713b26d4ec055c7251b
Kamontat Chantrachirathumrong <github@kamontat.net>
2019-10-12 13:23:17 +0700

    chore(test): update ava config, but it seem not work

--- ava.config.js
+++ ava.config.js
@@ -17,7 +17,13 @@ const isCI = process.env.CI === "true" || process.env.CI === true;
 // }
 const chooseByEnv = _opts => {
   const opts = Object.assign(
-    { ci: undefined, dev: undefined, test: undefined, prod: undefined, default: undefined },
+    {
+      ci: undefined,
+      dev: undefined,
+      test: undefined,
+      prod: undefined,
+      default: undefined,
+    },
     _opts,
   );
 
@@ -30,11 +36,25 @@ const chooseByEnv = _opts => {
 };
 
 const settings = {
-  cache: chooseByEnv({ default: true, prod: false }),
-  concurrency: chooseByEnv({ default: 5, prod: 1, ci: 10 }),
+  cache: chooseByEnv({
+    default: true,
+    prod: false,
+  }),
+  concurrency: chooseByEnv({
+    default: 5,
+    prod: 1,
+    ci: 10,
+  }),
   files: ["**/*.spec.*", "!**/_*.spec.*"],
   compileEnhancements: false,
-  verbose: chooseByEnv({ default: false, ci: true }),
+  verbose: chooseByEnv({
+    default: false,
+    ci: true,
+  }),
+  color: chooseByEnv({
+    default: true,
+    ci: false,
+  }),
   extensions: ["ts"],
   require: ["ts-node/register", "tsconfig-paths/register"],
   environmentVariables: {},
@@ -45,14 +65,9 @@ Environment:
   1. node: ${process.env.NODE_ENV}
   2. ci: ${isCI}
 Settings
-  1. cache: ${settings.cache}
-  2. concurrency: ${settings.concurrency}
-  3. files: ${settings.files}
-  4. compileEnhancements: ${settings.compileEnhancements}
-  5. verbose: ${settings.verbose}
-  6. extensions: ${settings.extensions}
-  7. require: ${settings.require}
-  8. environmentVariables: ${JSON.stringify(settings.environmentVariables)}
+${Object.keys(settings)
+  .map((k, i) => `  ${i + 1}. ${k}: ${JSON.stringify(settings[k])}`)
+  .join("\n")}
 `);
 
-module.exports.default = settings;
+export default settings;
