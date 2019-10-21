fc7566442eed2afb321ea9b2d28588cb01955379
Kamontat Chantrachirathumrong <developer@kamontat.net>
2019-10-21 10:05:53 +0700

    refac(database): change file name effected

--- packages/nd-database/models/__test__/DatabaseMock.ts
+++ packages/nd-database/models/__test__/DatabaseMock.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import IDatabase from "../IDatabase";
+import IDatabase from "../IDBO";
 
 /**
  * NOTES: Never tested; be aware when use this
