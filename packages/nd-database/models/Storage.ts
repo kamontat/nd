f557de2cde3defa66cbb4000f59d07e3780a83cf
Kamontat Chantrachirathumrong <developer@kamontat.net>
2019-10-21 10:06:47 +0700

    impr(database): make improvement on code level in both db and storage

--- packages/nd-database/models/Storage.ts
+++ packages/nd-database/models/Storage.ts
@@ -2,37 +2,14 @@ import firebase, { storage } from "firebase/app";
 import "firebase/storage";
 import ExceptionService, { ERR_DBO } from "@nd/error";
 
-import { IDatabase } from "..";
+import FBO from "./FBO";
 
-declare let __FIREBASE_API_KEY__: string;
-declare let __FIREBASE_AUTH_DOMAIN__: string;
-declare let __FIREBASE_DATABASE_URL__: string;
-declare let __FIREBASE_PROJECT_ID__: string;
-declare let __FIREBASE_STORAGE_BUCKET__: string;
-declare let __FIREBASE_MESSAGING_SENDER_ID__: string;
-declare let __FIREBASE_APP_ID__: string;
-
-export default class Storage implements IDatabase {
+export default class Storage extends FBO<any> {
   private _storage: storage.Storage;
 
   constructor() {
-    if (firebase.apps.length === 0) {
-      const app = firebase.initializeApp({
-        apiKey: process.env.NODE_ENV === "test" ? "" : __FIREBASE_API_KEY__,
-        authDomain: process.env.NODE_ENV === "test" ? "" : __FIREBASE_AUTH_DOMAIN__,
-        databaseURL: process.env.NODE_ENV === "test" ? "" : __FIREBASE_DATABASE_URL__,
-        projectId: process.env.NODE_ENV === "test" ? "" : __FIREBASE_PROJECT_ID__,
-        storageBucket: process.env.NODE_ENV === "test" ? "" : __FIREBASE_STORAGE_BUCKET__,
-        messagingSenderId: process.env.NODE_ENV === "test" ? "" : __FIREBASE_MESSAGING_SENDER_ID__,
-        appId: process.env.NODE_ENV === "test" ? "" : __FIREBASE_APP_ID__,
-      });
-
-      // LoggerService.log(LOGGER_FIREBASE, app);
-
-      this._storage = firebase.storage(app);
-    } else this._storage = firebase.storage();
-
-    // LoggerService.log(LOGGER_FIREBASE_STORAGE, this._storage);
+    super();
+    this._storage = firebase.storage(this.app);
   }
 
   /**
@@ -64,6 +41,9 @@ export default class Storage implements IDatabase {
     return this._storage.ref(`${ver}/${fileName}`).getDownloadURL();
   }
 
+  /**
+   * readonly storage
+   */
   public write() {
     return new Promise((_, rej) => rej(ExceptionService.build(ERR_DBO, "cannot write anything to storage server")));
   }
