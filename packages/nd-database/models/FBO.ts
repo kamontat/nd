98d8546d6fb96f4b725bc61be58e5402478c182a
Kamontat Chantrachirathumrong <developer@kamontat.net>
2019-10-21 10:05:32 +0700

    refac(database): in order to reduce duplicate code, I move constructor out

---
+++ packages/nd-database/models/FBO.ts
@@ -0,0 +1,34 @@
+import firebase, { firestore } from "firebase/app";
+
+import { IDBO } from "..";
+
+declare let __FIREBASE_API_KEY__: string;
+declare let __FIREBASE_AUTH_DOMAIN__: string;
+declare let __FIREBASE_DATABASE_URL__: string;
+declare let __FIREBASE_PROJECT_ID__: string;
+declare let __FIREBASE_STORAGE_BUCKET__: string;
+declare let __FIREBASE_MESSAGING_SENDER_ID__: string;
+declare let __FIREBASE_APP_ID__: string;
+
+export default abstract class FBO<T> implements IDBO<T> {
+  protected app: firebase.app.App;
+
+  constructor() {
+    if (firebase.apps.length === 0) {
+      this.app = firebase.initializeApp({
+        apiKey: process.env.NODE_ENV === "test" ? "" : __FIREBASE_API_KEY__,
+        authDomain: process.env.NODE_ENV === "test" ? "" : __FIREBASE_AUTH_DOMAIN__,
+        databaseURL: process.env.NODE_ENV === "test" ? "" : __FIREBASE_DATABASE_URL__,
+        projectId: process.env.NODE_ENV === "test" ? "" : __FIREBASE_PROJECT_ID__,
+        storageBucket: process.env.NODE_ENV === "test" ? "" : __FIREBASE_STORAGE_BUCKET__,
+        messagingSenderId: process.env.NODE_ENV === "test" ? "" : __FIREBASE_MESSAGING_SENDER_ID__,
+        appId: process.env.NODE_ENV === "test" ? "" : __FIREBASE_APP_ID__,
+      });
+    } else {
+      this.app = firebase.app();
+    }
+  }
+
+  public abstract read(path: string): Promise<T>;
+  public abstract write(path: string, value: any): Promise<T>;
+}
